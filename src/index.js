fe6289c6c14397ced1e19f0af58daf7624b6a91a
Joe <Walktheworld@me.com>
2021-05-27 11:13:48 -0600

    adjusted li to ul to remove bullets

--- src/index.js
+++ src/index.js
@@ -48,34 +48,41 @@ function renderIng(e){
         const drinkContainer= document.getElementById('drink-container')
         const ulTag = document.createElement('ul')
     
-        const ingTag1 = document.createElement('li')
-        const measTag1 = e.strMeasure1
-        const ing1 = e.strIngredient1
+        const ingTag1 = document.createElement('ul')
+        const measTag1 = e.strMeasure1 || ""
+        const ing1 = e.strIngredient1 || ""
         ingTag1.innerText = `${measTag1} ${ing1}`
         
 
-        const ingTag2 = document.createElement('li')
-        const measTag2 = e.strMeasure2
-        const ing2 = e.strIngredient2
+        const ingTag2 = document.createElement('ul')
+        const measTag2 = e.strMeasure2 || ""
+        const ing2 = e.strIngredient2 || ""
         ingTag2.innerText = `${measTag2} ${ing2}`
 
-        const ingTag3 = document.createElement('li')
-        const measTag3 = e.strMeasure3
-        const ing3 = e.strIngredient3
+        const ingTag3 = document.createElement('ul')
+        const measTag3 = e.strMeasure3 || ""
+        const ing3 = e.strIngredient3 || ""
         ingTag3.innerText = `${measTag3} ${ing3}`
 
-        const ingTag4 = document.createElement('li')
-        const measTag4 = e.strMeasure4
-        const ing4 = e.strIngredient4
+        const ingTag4 = document.createElement('ul')
+        const measTag4 = e.strMeasure4 || ""
+        const ing4 = e.strIngredient4 || ""
         ingTag4.innerText = `${measTag4} ${ing4}`
 
-        const ingTag5 = document.createElement('li')
-        ingTag5.innerText = e.strIngredient5
+        const ingTag5 = document.createElement('ul')
+        const measTag5 = e.strMeasure5 || ""
+        const ing5 = e.strIngredient5 || ""
+        ingTag5.innerText = `${measTag5} ${ing5}`
+
+        const ingTag6 = document.createElement('ul')
+        const measTag6 = e.strMeasure6 || ""
+        const ing6 = e.strIngredient6 || ""
+        ingTag6.innerText = `${measTag6} ${ing6}`
 
         const insTag = document.createElement('p')
         insTag.innerText = e.strInstructions
     
-        ulTag.append(ingTag1, ingTag2, ingTag3, ingTag4, ingTag5, insTag)
+        ulTag.append(ingTag1, ingTag2, ingTag3, ingTag4, ingTag5, ingTag6, insTag)
         drinkContainer.appendChild(ulTag)
 
         const cheersBtn = document.createElement("button")
